3568dfeb573cc2ec8322556243705fd08035a8eb
maplecheng <maplecheng1313@gmail.com>
2020-07-25 17:07:40 +0800

    read init

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import { Router, Route, Switch, Redirect } from 'dva/router';
 // custom components
 import Main from './routes/Main';
 import New from './routes/New';
+import Read from './routes/Read';
 import Err404 from './routes/Exception/Err404';
 
 function RouterConfig({ history }) {
@@ -12,6 +13,7 @@ function RouterConfig({ history }) {
       <Switch>
         <Route exact path='/' component={Main} />
         <Route exact path='/new' component={New} />
+        <Route exact path='/read' component={Read} />
         <Route exact path='/404' component={Err404} />
         <Route render={() => (
           <Redirect to='/404' />
