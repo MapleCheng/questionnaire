a56e945536ab524ba5826cb6824286fa8668eead
maplecheng <maplecheng1313@gmail.com>
2020-07-26 23:01:30 +0800

    true or false

--- src/components/QuestionsView/TrueOrFalse.js
+++ src/components/QuestionsView/TrueOrFalse.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Form, Radio, Row, Col } from 'antd';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons'
 
 class TrueOrFalse extends Component {
   render() {
+
+    const { name } = this.props;
     return (
-      <div>
-        
-      </div>
+      <Form.Item name={`${name}/TrueOrFalse`}>
+        <Radio.Group style={{width: '100%'}}>
+          <Row justify='space-between'>
+            <Col span={11}>
+              <Radio.Button value={0} style={{width: '100%', textAlign: 'center'}} ><CheckCircleOutlined /></Radio.Button>
+            </Col>
+            <Col span={11}>
+              <Radio.Button value={1} style={{width: '100%', textAlign: 'center'}} ><CloseCircleOutlined /></Radio.Button>
+            </Col>
+          </Row>
+        </Radio.Group>
+      </Form.Item>
     );
   }
 }
