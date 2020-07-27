bb042fdd4718231f03caab2ec589838a8e886ab0
maplecheng <maplecheng1313@gmail.com>
2020-07-27 09:37:39 +0800

    new question

--- src/utils/dataConsolidation.js
+++ src/utils/dataConsolidation.js
@@ -1,45 +1,47 @@
 
 
 
-export default (data, r) => {
+export default {
+  // 新增問卷合併data
+  newQuestion: (data, r) => {
+
+    const questions = r.map((item, key) => {
+
+      const mode = data[`${item.name}/mode`];
+
+      let result = {
+        question: data[`${item.name}/question`],
+        mode,
+      }
+
+      if (mode === 0) {
+
+        result.consent_survey = [];
+
+        data[`${item.name}/consent_survey`].forEach(element => {
+          if (element) {
+            result.consent_survey.push(element);
+          }
+        });
+      } else if (mode === 1 || mode === 2) {
+
+        result.choice = [];
+
+        data[`${item.name}/choice`].forEach(element => {
+          if (element) {
+            result.choice.push(element);
+          }
+        });
+      }
+
+      return result;
+    })
 
 
-  const questions = r.map((item, key) => {
-
-    const mode = data[`${item.name}/mode`];
-
-    let result = {
-      question: data[`${item.name}/question`],
-      mode,
+    return {
+      title: data.title,
+      description: data.description,
+      questions,
     }
-
-    if (mode === 0) {
-
-      result.consent_survey = [];
-
-      data[`${item.name}/consent_survey`].forEach(element => {
-        if (element) {
-          result.consent_survey.push(element);
-        }
-      });
-    } else if (mode === 1 || mode === 2) {
-
-      result.choice = [];
-
-      data[`${item.name}/choice`].forEach(element => {
-        if (element) {
-          result.choice.push(element);
-        }
-      });
-    }
-
-    return result;
-  })
-
-
-  return {
-    title: data.title,
-    description: data.description,
-    questions,
   }
 }
